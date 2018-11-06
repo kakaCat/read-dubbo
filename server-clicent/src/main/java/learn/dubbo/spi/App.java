be675739bc24a9f41d7d4067f665a0aad263acab
suantou <suantou@helijia.com>
2018-11-06 16:17:14 +0800

    搭建

--- server-clicent/src/main/java/learn/dubbo/spi/App.java
+++ server-clicent/src/main/java/learn/dubbo/spi/App.java
@@ -1,19 +1,25 @@
 package learn.dubbo.spi;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import read.dubbo.ITest;
 
 /**
  * Created by JAVA on 2018/8/12.
  */
 public class App {
 
+
+
     public static void main(String[] args) {
-
-
-
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext
                 (new String[] {"dubbo-client.xml"});
+
+        ITest test = (ITest)context.getBean("test"); // 获取远程服务代理
+
+        String msg = test.iTest("--");
+
+        System.out.println(msg);
     }
 
 
--- server-provider/src/main/java/read/dubbo/impl/Test.java
+++ server-provider/src/main/java/read/dubbo/impl/Test.java
@@ -1,10 +1,12 @@
 package read.dubbo.impl;
 
+import org.springframework.stereotype.Service;
 import read.dubbo.ITest;
 
 /**
  * Created by JAVA on 2018/7/30.
  */
+@Service
 public class Test implements ITest {
     @Override
     public String iTest(String msg) {
